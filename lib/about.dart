0851c90d7110ca1f6ec7aaddd4f548f09744b7ae
techyminati <sinha.aryan03@gmail.com>
2023-06-05 18:04:05 +0530

    windy: Remove un-used imports
    Signed-off-by: techyminati <sinha.aryan03@gmail.com>

--- lib/about.dart
+++ lib/about.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
-import 'dart:convert';
 import 'package:intl/intl.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:shared_preferences/shared_preferences.dart';
--- lib/key_sample.dart
+++ lib/key_sample.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
-import 'dart:convert';
 // Secret API Key
 // Add your openweathermap api key here
 const apiKey = "xxxxxxxxxxxxxxxxxxxxxxxx";
