466f20d4ebdeb3823e2f6ee7e76136e0f9c9ae88
techyminati <sinha.aryan03@gmail.com>
2023-09-24 17:48:34 +0530

    windy: Version 1.0.2
    Signed-off-by: techyminati <sinha.aryan03@gmail.com>

--- lib/about.dart
+++ lib/about.dart
@@ -39,7 +39,7 @@ class AboutPage extends StatelessWidget {
             SizedBox(height: 8),
             // Display app version
             Text(
-              'Version 1.0.1',
+              'Version 1.0.2',
               style: Theme.of(context).textTheme.caption,
             ),
             Spacer(),
